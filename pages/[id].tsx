2833a648d0bfeddc7c18d17af27695153174ccd7
Kaffff <takahashi.prog@gmail.com>
2022-03-29 23:39:31 +0900

    edit design of article page

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -6,23 +6,48 @@ import { Header } from "../stories/Header";
 import { join } from "path";
 import Image from "next/image";
 
+const Tags: React.FC<{ tags: string }> = ({ tags }) => {
+  const taglist = tags.trimEnd().split(new RegExp(" |　"));
+
+  return (
+    <div className=" flex flex-wrap gap-2 justify-center">
+      {Array.from(new Set(taglist)).map((name) => (
+        <Tag key={name} name={name} />
+      ))}
+    </div>
+  );
+};
+
+const Tag: React.FC<{ name: string }> = ({ name }) => {
+  return (
+    <button className="text-white text-sm inline-block border px-2 py-1 rounded-sm hover:border-emerald-200 hover:text-emerald-200 ">
+      # {name}
+    </button>
+  );
+};
+
 const Article: NextPage<Content> = (content) => {
+  const date = new Date(content.date);
   return (
     <div className="h-full flex flex-col min-h-screen font-shippori">
       <Header />
-      <main className="flex-grow bg-gray-900 place-self-center max-w-7xl px-6 pt-14 pb-32">
-        <Image
-          src={content.thumbnail.url}
-          width="1000"
-          height="500"
-          objectFit="cover"
-        ></Image>
-        <div className="text-white text-4xl text-center pt-10 pb-6">
+      <Image
+        className="opacity-80 duration-500 hover:scale-110 hover:opacity-100"
+        src={content.thumbnail.url}
+        width="800"
+        height="500"
+        layout="responsive"
+      ></Image>
+      <main className="flex-grow bg-gray-900 place-self-center max-w-7xl px-6 pb-32">
+        <div className=" text-white text-4xl text-center pt-10 pb-4">
           {content.title}
         </div>
-        <div className="text-white text-sm text-center">{content.date}</div>
+        <div className="pb-2 text-white text-sm text-center">{`${date.getFullYear()}年${
+          date.getMonth() + 1
+        }月${date.getDate()}日`}</div>
+        <Tags tags={content.tag} />
         <div
-          className="text-gray-100 pt-20"
+          className="text-gray-100 pt-6 text-justify"
           dangerouslySetInnerHTML={{ __html: content.body }}
         ></div>
       </main>
--- stories/ArticleCard.tsx
+++ stories/ArticleCard.tsx
@@ -8,19 +8,24 @@ export const ArticleCard: React.FC<{
   thumbnail: string;
   title: string;
   publishedAt: string;
-}> = ({ id, thumbnail, title, publishedAt }) => (
-  <Link href={join("/", id)}>
-    <div className="w-full flex flex-col duration-300 hover:scale-105 hover:cursor-pointer ">
-      <Image
-        className="rounded-lg"
-        src={thumbnail}
-        width="200"
-        height="150"
-        objectFit="cover"
-        alt="picture of mountain"
-      />
-      <div className="text-white text-lg pt-2">{title}</div>
-      <div className="text-gray-400">{publishedAt}</div>
-    </div>
-  </Link>
-);
+}> = ({ id, thumbnail, title, publishedAt }) => {
+  const date = new Date(publishedAt);
+  return (
+    <Link href={join("/", id)}>
+      <div className="w-full flex flex-col duration-300 hover:scale-105 hover:cursor-pointer ">
+        <Image
+          className="rounded-lg"
+          src={thumbnail}
+          width="200"
+          height="150"
+          objectFit="cover"
+          alt="picture of mountain"
+        />
+        <div className="text-white text-lg pt-2">{title}</div>
+        <div className="text-gray-400">{`${date.getFullYear()}年${
+          date.getMonth() + 1
+        }月${date.getDate()}日`}</div>
+      </div>
+    </Link>
+  );
+};

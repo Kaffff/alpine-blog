e6fe9e508b45c8382e0d47ae30ef9607884fc94b
Kaffff <takahashi.prog@gmail.com>
2022-04-11 04:11:05 +0900

    edit style

--- stories/ArticleCard.tsx
+++ stories/ArticleCard.tsx
@@ -12,28 +12,31 @@ export const ArticleCard: React.FC<{
   const _date = new Date(date);
   return (
     <Link href={join("/", id)} passHref>
-      <div className="flex flex-row w-full duration-300 hover:scale-105 hover:cursor-pointer">
-        <div className="pr-2 w-2/5">
-          <Image
-            className="rounded"
-            src={`${thumbnail}?fit=crop&crop=edge&w=302&h=125`}
-            width="1207"
-            height="500"
-            layout="responsive"
-            objectFit="cover"
-            alt="picture of mountain"
-            lazyBoundary="400px"
-            priority={true}
-          />
-        </div>
-        <div className="w-3/5">
-          <div className="pl-2 text-xl font-bold  mobile:text-base">
-            {title}
+      <div>
+        <div className=" flex flex-row w-full duration-300 hover:scale-105 hover:cursor-pointer">
+          <div className="pr-2 w-2/5">
+            <Image
+              className="rounded"
+              src={`${thumbnail}?fit=crop&crop=edge&w=302&h=125`}
+              width="1207"
+              height="500"
+              layout="responsive"
+              objectFit="cover"
+              alt="picture of mountain"
+              lazyBoundary="400px"
+              priority={true}
+            />
           </div>
-          <div className="pt-1 pl-2 text-zinc-400 mobile:text-xs">{`${_date.getFullYear()}年${
-            _date.getMonth() + 1
-          }月${_date.getDate()}日`}</div>
+          <div className="w-3/5">
+            <div className="pl-2 text-xl font-bold  mobile:text-base">
+              {title}
+            </div>
+            <div className="pt-1 pl-2 text-zinc-400 mobile:text-xs">{`${_date.getFullYear()}年${
+              _date.getMonth() + 1
+            }月${_date.getDate()}日`}</div>
+          </div>
         </div>
+        <div className="mt-3 w-full h-px bg-zinc-600"></div>
       </div>
     </Link>
   );
